e29f5acf2275f1cad1314f840c137645546739aa
takdev <takfarinas.yazid@gmail.com>
2023-01-24 11:45:59 +0100

    module: exemple figure

--- date.js
+++ date.js
@@ -1,7 +1,27 @@
+ 
 
-$(
-    $("load",()=>{
-       $("#date").html("ilest "+new Date())
-    })
-);
+var Figure = (function () {
+    if (!figure.monAire)
+        figure.monAire = function () {
+            return this.largeur * this.hauteur;
+        }
+    if (!figure.centre)
+        figure.centre = function () {
+            return [
+                (this.x + this.largeur) / 2, (this.y + this.hauteur) / 2
+            ];
+        }
 
+    return function (x, y, largeur, hauteur) {
+        this.x = x;
+        this.y = y;
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+
+        for (var p in figure) {
+            if (typeof figure[p] == "function")
+                this[p] = figure[p];
+        }
+
+    }
+})(figure);
